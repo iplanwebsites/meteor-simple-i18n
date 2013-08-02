78c252c032fa83974f1b9f01ccd91894a9bd3eae
I Plan Websites <info@iplanwebsites.com>
2013-08-02 15:57:16 -0400

    Update i18n-view-helper.js

--- i18n-view-helper.js
+++ i18n-view-helper.js
@@ -16,11 +16,18 @@
   // &lt;/template&gt;</pre>
   //
   // @see `I18n.prototype.t`
-  Handlebars.registerHelper('t', function(object){
+   Handlebars.registerHelper('t', function(object){ 
+    if(! Meteor.i18n){
+      return '...'; // return dummy string before we can access the i18n global instance
+      var i18n = Meteor.I18n(OPTIONS_i18n) ;// create a new instance (inneficient)
+    }else{
+      var i18n = Meteor.i18n;
+    }
+    
     var args = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
-    args[0] = Meteor.I18n().t(args[0]);
+    args[0] = i18n.t(args[0]);
     if (args.length > 1) {
-      return Meteor.I18n().sprintf.apply(Meteor.I18n().sprintf, args);
+      return i18n.sprintf.apply(i18n.sprintf, args);
     } else {
       return args[0];
     }
